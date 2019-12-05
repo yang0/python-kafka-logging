3340263f7af44924328cfb7c73b3d5afe15590a7
git <yang0@126.com>
2019-12-05 09:50:43 +0800

    bugfix

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@
 import os
 import sys
 import uuid
-from pip.req import parse_requirements
 from codecs import open
 
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 try:
     from setuptools import setup
